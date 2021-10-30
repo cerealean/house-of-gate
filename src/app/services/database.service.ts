bb5f44ebd7b89734026355029266575a74d5b4f5
Michael Crawford <cerealean@me.com>
2021-10-30 02:18:20 -0500

    Minor changes

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -17,27 +17,26 @@ export class DatabaseService {
       encounters: "++id,&name,date,campaignId,*monsterIds"
     });
 
-    this.db.on('ready', () => {
-      return this.db.table('monsters').count().then(numberOfMonsters => {
-        if (numberOfMonsters > 0) {
-          return;
-        } else {
-          return new Promise(resolve => {
-            const worker = new Worker(
-              new URL('../data/db-initializer.worker', import.meta.url),
-              { type: 'module' }
-            );
-            worker.onmessage = async ({ data }) => {
-              resolve(data);
-            };
-            worker.postMessage('');
-          }).then(data => {
-            const monsters = (data as MonsterInfo[]).map(mi => new Monster(mi));
-            const bulkAdd = this.db.table('monsters').bulkAdd(monsters);
-            return bulkAdd;
-          });
-        }
-      });
+    this.db.on('ready', async () => {
+      const numberOfMonsters = await this.db.table('monsters').count();
+      if (numberOfMonsters > 0) {
+        return;
+      } else {
+        return new Promise(resolve => {
+          const worker = new Worker(
+            new URL('../data/db-initializer.worker', import.meta.url),
+            { type: 'module' }
+          );
+          worker.onmessage = async ({ data }) => {
+            resolve(data);
+          };
+          worker.postMessage('');
+        }).then(monsterInfo => {
+          const monsters = (monsterInfo as MonsterInfo[]).map(mi => new Monster(mi));
+          const bulkAdd = this.db.table('monsters').bulkAdd(monsters);
+          return bulkAdd;
+        });
+      }
     });
     this.db.open();
   }
