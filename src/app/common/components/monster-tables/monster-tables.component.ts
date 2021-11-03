d86582a860e3a1212cc961b73e6c180d2e19d3c8
Michael Crawford <cerealean@me.com>
2021-11-02 21:29:53 -0500

    Removed unnecessary eventing from monster tables component

--- src/app/common/components/monster-tables/monster-tables.component.ts
+++ src/app/common/components/monster-tables/monster-tables.component.ts
@@ -1,8 +1,7 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ReplaySubject, Subscription } from 'rxjs';
 import { ColumnInfo } from 'src/app/monsters/models/column-info';
 import { Monster } from 'src/app/monsters/models/monster';
 import { StorageKeys, StorageService } from 'src/app/services/storage.service';
@@ -12,24 +11,13 @@ import { StorageKeys, StorageService } from 'src/app/services/storage.service';
   templateUrl: './monster-tables.component.html',
   styleUrls: ['./monster-tables.component.scss']
 })
-export class MonsterTablesComponent implements OnInit, OnDestroy, AfterViewInit, OnChanges {
+export class MonsterTablesComponent implements OnInit, AfterViewInit, OnChanges {
   @Input() public monsters: Monster[] = [];
   public displayedColumns: string[] = [];
   public columns: ColumnInfo[] = this.generateInitialColumns();
-
   public dataSource!: MatTableDataSource<Monster>;
 
   @Output() isLoading = new EventEmitter<boolean>();
-
-  private _columnUpdates$ = new ReplaySubject<{
-    updated: ColumnInfo | null,
-    allColumns: ColumnInfo[],
-    shownColumns: ColumnInfo[]
-  }>(20);
-
-  private columnUpdates$ = this._columnUpdates$.asObservable();
-
-  private subscriptions = new Subscription();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -43,21 +31,9 @@ export class MonsterTablesComponent implements OnInit, OnDestroy, AfterViewInit,
     if(storedColumns) {
       this.columns = storedColumns;
     }
+    this.refreshDisplayedColumns();
     this.isLoading.emit(true);
     this.dataSource = new MatTableDataSource<Monster>([]);
-    this.subscriptions.add(
-      this.columnUpdates$.subscribe(update => {
-        this.columns = update.allColumns;
-        this.displayedColumns = update.shownColumns
-          .filter(c => c.isShown)
-          .map(c => c.name);
-      })
-    );
-    this._columnUpdates$.next({
-      allColumns: this.columns.slice(),
-      shownColumns: this.columns.filter(c => c.isShown),
-      updated: null
-    });
   }
 
   ngOnChanges(simpleChanges: SimpleChanges): void {
@@ -73,10 +49,6 @@ export class MonsterTablesComponent implements OnInit, OnDestroy, AfterViewInit,
     this.dataSource.data = this.monsters;
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
-
   isArray(data: any): boolean {
     return Array.isArray(data);
   }
@@ -88,16 +60,18 @@ export class MonsterTablesComponent implements OnInit, OnDestroy, AfterViewInit,
 
   toggleColumn(column: ColumnInfo): void {
     column.isShown = !column.isShown;
+    this.refreshDisplayedColumns();
     this.storage.setData(StorageKeys.ColumnInfo, this.columns.slice());
-    this._columnUpdates$.next({
-      allColumns: this.columns.slice(),
-      shownColumns: this.columns.filter(c => c.isShown),
-      updated: column
-    });
   }
 
   stopPropagation($event: Event) {
     $event.stopPropagation();
+  }
+
+  private refreshDisplayedColumns(): void {
+    this.displayedColumns = this.columns
+      .filter(c => c.isShown)
+      .map(c => c.name);
   }
 
   private generateInitialColumns(): ColumnInfo[] {
