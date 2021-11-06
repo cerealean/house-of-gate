0639f805a89a849a493712cf10b7c0a8066db3cf
Michael Crawford <cerealean@me.com>
2021-11-05 23:19:55 -0500

    Added beta notice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ServiceWorkerService } from './services/service-worker.service';
 
 @Component({
@@ -6,11 +7,17 @@ import { ServiceWorkerService } from './services/service-worker.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public readonly currentYear = new Date().getFullYear();
 
   constructor(
-    private readonly _serviceWorker: ServiceWorkerService
+    private readonly _serviceWorker: ServiceWorkerService,
+    private readonly _snackBar: MatSnackBar
   ) { }
+
+  ngOnInit(): void {
+    const snackBar = this._snackBar
+      .open('Notice: This app is in beta and under development; features may change at any time. Thank you for using the app! Please direct any feedback to admin@houseofgate.com.', 'Dismiss');
+  }
 
 }
