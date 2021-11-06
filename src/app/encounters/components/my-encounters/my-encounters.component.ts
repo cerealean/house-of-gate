8dfa593bc4bf47e5bb6379640395d7237be273e5
Michael Crawford <cerealean@me.com>
2021-11-05 23:01:46 -0500

    Finished the my encounters saving and displaying, for now

--- src/app/encounters/components/my-encounters/my-encounters.component.ts
+++ src/app/encounters/components/my-encounters/my-encounters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Campaign } from 'src/app/campaigns/models/campaign';
 import { CampaignDataService } from 'src/app/data/services/campaigns/campaign-data.service';
 import { Encounter } from '../../models/encounter';
@@ -13,12 +14,32 @@ export class MyEncountersComponent implements OnInit {
   public selectedCampaign?: Campaign;
   public selectedEncounter?: Encounter;
 
+  private givenCampaignId?: number;
+
   constructor(
-    private readonly campaignData: CampaignDataService
-  ) { }
+    private readonly campaignData: CampaignDataService,
+    router: Router
+  ) {
+    this.givenCampaignId = router.getCurrentNavigation()?.extras?.state?.campaignId;
+  }
 
   async ngOnInit(): Promise<void> {
     this.campaigns = await this.campaignData.getAllCampaigns();
+
+    if(this.givenCampaignId) {
+      this.selectedCampaign = this.campaigns.find(c => c.id === this.givenCampaignId);
+    }
+    else if(this.campaigns.length === 1) {
+      this.selectedCampaign = this.campaigns[0];
+    }
+  }
+
+  onCampaignChange(campaign: Campaign) {
+    if(campaign.encounters.length === 1) {
+      this.selectedEncounter = campaign.encounters[0];
+    } else {
+      this.selectedEncounter = undefined;
+    }
   }
 
 }
