98060a48d1949dec26f15dcb3107f3bd683e7d71
Michael Crawford <michaelleacrawford@gmail.com>
2022-01-29 18:27:02 -0600

    Added character sheet

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -16,12 +16,14 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatSelectModule } from '@angular/material/select';
+import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
 
 
 @NgModule({
   declarations: [
     CharactersComponent,
-    NewCharacterComponent
+    NewCharacterComponent,
+    CharacterSheetComponent
   ],
   imports: [
     CommonModule,
