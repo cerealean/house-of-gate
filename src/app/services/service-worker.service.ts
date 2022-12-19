6b1d95b5894c9931c3d31f3a71cb2ae50e7a0d21
Michael Crawford <michael.crawford@veteransunited.com>
2022-12-19 09:33:17 -0600

    Simplified the version update code

--- src/app/services/service-worker.service.ts
+++ src/app/services/service-worker.service.ts
@@ -12,7 +12,6 @@ import { StorageKeys, StorageService } from './storage.service';
   providedIn: 'root'
 })
 export class ServiceWorkerService {
-
   private readonly subscriptions = new Subscription();
 
   constructor(
@@ -26,16 +25,12 @@ export class ServiceWorkerService {
     if (environment.production) {
       this.setupUpdateActions();
       this.setupUpdateChecks();
-      (window as any).sunshine = {
-        checkForUpdate: () => updates.checkForUpdate()
-      };
     }
   }
 
   private setupUpdateActions() {
-    this.updates.versionUpdates.subscribe(evt => {
-      if(evt.type === 'VERSION_DETECTED') {
-      } else if (evt.type === 'VERSION_READY') {
+    const versionUpdates$ = this.updates.versionUpdates.subscribe(evt => {
+      if (evt.type === 'VERSION_READY') {
         const snackbar = this.snackBar.open('An update is available! Reload the app?', 'Update!');
         snackbar.onAction().subscribe(() => {
           snackbar.instance.data.action = 'Updating...';
@@ -55,34 +50,8 @@ export class ServiceWorkerService {
         });
       }
     });
-    const available$ = this.updates.available.subscribe(() => {
-      const snackbar = this.snackBar.open('An update is available! Reload the app?', 'Update!');
-      const action$ = snackbar.onAction().subscribe(() => {
-        snackbar.instance.data.action = 'Updating...';
-        this.updates.activateUpdate().then(() => {
-          this.storage.clearData(StorageKeys.EncounterFilters);
-          this.storage.clearData(StorageKeys.MonsterFilters);
-          this.storage.clearData(StorageKeys.SpellFilters);
-          this.storage.clearData(StorageKeys.EncounterGeneratorMonsterFilters);
-          this.storage.clearData(StorageKeys.PreviouslyGeneratedEncounters);
-          Promise.all([
-            this.monsterData.clearMonsters(),
-            this.spellData.clearSpells()
-          ]).finally(() => {
-            document.location.reload();
-          });
-        });
-      });
-      this.subscriptions.add(action$);
-    });
 
-    const activated$ = this.updates.activated.subscribe(() => {
-      const updatedMessage = `The app has finished updating. Search filters have been reset to ensure data compatibility.`;
-      this.snackBar.open(updatedMessage, 'Ok');
-    });
-
-    this.subscriptions.add(available$);
-    this.subscriptions.add(activated$);
+    this.subscriptions.add(versionUpdates$);
   }
 
   private setupUpdateChecks() {
