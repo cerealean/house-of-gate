b8593fcdc98131ada07a4858f645f7d8c4b06226
Michael Crawford <michael.crawford@veteransunited.com>
2022-12-20 14:52:43 -0600

    Making sure to unsubscribe from value changes

--- src/app/can-i-hit/can-i-hit.component.ts
+++ src/app/can-i-hit/can-i-hit.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UntypedFormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-can-i-hit',
   templateUrl: './can-i-hit.component.html',
   styleUrls: ['./can-i-hit.component.scss']
 })
-export class CanIHitComponent implements OnInit {
+export class CanIHitComponent implements OnInit, OnDestroy {
 
   public readonly elevationDifferenceCtrl = new UntypedFormControl(5, [Validators.min(0), Validators.required, Validators.pattern(/^[0-9]*$/)]);
   public readonly horizontalDifferenceCtrl = new UntypedFormControl(10, [Validators.min(0), Validators.required, Validators.pattern(/^[0-9]*$/)]);
 
   public distance = Math.hypot(this.elevationDifferenceCtrl.value, this.horizontalDifferenceCtrl.value);
 
+  private readonly subscriptions = new Subscription();
+
   constructor() { }
 
   ngOnInit(): void {
-    this.elevationDifferenceCtrl.valueChanges.subscribe(() => this.recalculate());
-    this.horizontalDifferenceCtrl.valueChanges.subscribe(() => this.recalculate());
+    this.subscriptions.add(this.elevationDifferenceCtrl.valueChanges.subscribe(() => this.recalculate()));
+    this.subscriptions.add(this.horizontalDifferenceCtrl.valueChanges.subscribe(() => this.recalculate()));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   private recalculate() {
