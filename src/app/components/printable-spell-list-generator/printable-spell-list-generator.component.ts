4bb306ab2e6ab3a6c6690d2c55214cb2605c66ba
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2024-05-09 13:49:18 -0500

    Added new component for spell list generator

---
+++ src/app/components/printable-spell-list-generator/printable-spell-list-generator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-printable-spell-list-generator',
+  standalone: true,
+  imports: [],
+  templateUrl: './printable-spell-list-generator.component.html',
+  styleUrl: './printable-spell-list-generator.component.scss'
+})
+export class PrintableSpellListGeneratorComponent {
+
+}
