c736c407c01c17c24114f28d7aafc3d12d3e84dc
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2024-05-09 14:40:43 -0500

    Added classes related to spells and updated the model

--- src/app/spells/models/spell.ts
+++ src/app/spells/models/spell.ts
@@ -1,6 +1,6 @@
-import { IStoredMetadata } from "src/app/data/models/i-stored-metadata";
-import { Classes } from "src/app/data/static/classes";
-import { SchoolsOfMagic } from "src/app/data/static/schools-of-magic";
+import { IStoredMetadata } from 'src/app/data/models/i-stored-metadata';
+import { Classes, SpellsClasses } from 'src/app/data/static/classes';
+import { SchoolsOfMagic } from 'src/app/data/static/schools-of-magic';
 
 export interface ISpell extends IStoredMetadata {
   id: string;
@@ -28,17 +28,24 @@ export class Spell implements ISpell {
   range!: string;
   components!: string;
   duration!: string;
-  classes!: Classes[];
+  classes!: SpellsClasses[];
   sources!: string[];
   text!: string;
   concentration!: boolean;
 
   public hasSourceIntersection(sources: string[]) {
-    return sources.some(givenSource => this.sources.some(s => s?.toLowerCase().includes(givenSource?.toLowerCase())));
+    return sources.some((givenSource) =>
+      this.sources.some((s) =>
+        s?.toLowerCase().includes(givenSource?.toLowerCase())
+      )
+    );
   }
 
   public hasClassIntersection(classes: Classes[]) {
-    return classes.some(givenClass => this.classes.some(s => s?.toLowerCase().includes(givenClass?.toLowerCase())));
+    return classes.some((givenClass) =>
+      this.classes.some((s) =>
+        s?.toLowerCase().includes(givenClass?.toLowerCase())
+      )
+    );
   }
-
 }
