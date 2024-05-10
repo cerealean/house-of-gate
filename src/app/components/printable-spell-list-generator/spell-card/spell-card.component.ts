dd10d31883ca2c5fbf695f1b2c8879990d25cea3
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2024-05-10 12:39:59 -0500

    Add beginnings of spell card component

--- src/app/components/printable-spell-list-generator/spell-card/spell-card.component.ts
+++ src/app/components/printable-spell-list-generator/spell-card/spell-card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+
+import { Spell } from 'src/app/spells/models/spell';
+
+import { OrdinalPipe } from '../../../pipes/ordinal/ordinal.pipe';
+import { YesNoPipe } from '../../../pipes/yes-no/yes-no.pipe';
 
 @Component({
   selector: 'app-spell-card',
   standalone: true,
-  imports: [],
   templateUrl: './spell-card.component.html',
-  styleUrl: './spell-card.component.scss'
+  styleUrl: './spell-card.component.scss',
+  imports: [OrdinalPipe, TitleCasePipe, YesNoPipe],
 })
 export class SpellCardComponent {
-
+  public readonly spell = input.required<Spell>();
 }
