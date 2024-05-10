9dede0453d1c238e3abb45e1f0925bd49fb4bfa6
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2024-05-10 12:06:14 -0500

    Created a spell card component to display spell info

---
+++ src/app/components/printable-spell-list-generator/spell-card/spell-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spell-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './spell-card.component.html',
+  styleUrl: './spell-card.component.scss'
+})
+export class SpellCardComponent {
+
+}
