f94b68894f122d33cf2dc02893cf715d28c6ca91
Michael Crawford <cerealean@users.noreply.github.com>
2024-06-10 23:09:23 -0500

    Move can-i-hit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,100 +1,100 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from "./components/home/home.component";
 
 export const routes: Routes = [
   {
-    path: 'monsters',
-    loadComponent: () =>
-      import('./monsters/components/monsters/monsters.component').then(
-        (m) => m.MonstersComponent
-      ),
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home",
   },
   {
-    path: 'encounters',
-    loadChildren: () =>
-      import('./encounters/encounters.routes').then((e) => e.routes),
-  },
-  {
-    path: 'campaigns',
-    loadComponent: () =>
-      import('./campaigns/campaigns-landing/campaigns-landing.component').then(
-        (e) => e.CampaignsLandingComponent
-      ),
-  },
-  {
-    path: 'spells',
-    loadComponent: () =>
-      import('./spells/components/spells/spells.component').then(
-        (e) => e.SpellsComponent
-      ),
-  },
-  {
-    path: 'feedback',
-    loadComponent: () =>
-      import('./feedback/feedback.component').then((m) => m.FeedbackComponent),
-  },
-  {
-    path: 'privacy',
-    loadComponent: () =>
-      import('./components/privacy-policy/privacy-policy.component').then(
-        (c) => c.PrivacyPolicyComponent
-      ),
-  },
-  {
-    path: 'terms',
-    loadComponent: () =>
-      import('./components/terms/terms.component').then(
-        (c) => c.TermsComponent
-      ),
-  },
-  {
-    path: 'items',
-    loadComponent: () =>
-      import('./items/items.component').then((i) => i.ItemsComponent),
-  },
-  {
-    path: 'magic-item-tables',
-    loadComponent: () =>
-      import('./magic-item-tables/magic-item-tables.component').then(
-        (c) => c.MagicItemTablesComponent
-      ),
-  },
-  {
-    path: 'printable-spell-list-generator',
-    loadComponent: () =>
-      import(
-        './components/printable-spell-list-generator/printable-spell-list-generator.component'
-      ).then((c) => c.PrintableSpellListGeneratorComponent),
-  },
-  {
-    path: 'home',
+    path: "home",
     component: HomeComponent,
   },
   {
-    path: 'changelog',
+    path: "monsters",
     loadComponent: () =>
-      import('./components/changelog/changelog.component').then(
-        (c) => c.ChangelogComponent
+      import("./monsters/components/monsters/monsters.component").then(
+        m => m.MonstersComponent
       ),
   },
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home',
-  },
-  {
-    path: 'can-i-hit',
-    loadComponent: () =>
-      import('./can-i-hit/can-i-hit.component').then((m) => m.CanIHitComponent),
-  },
-  {
-    path: 'characters',
+    path: "encounters",
     loadChildren: () =>
-      import('./characters/characters.routes').then((m) => m.routes),
+      import("./encounters/encounters.routes").then(e => e.routes),
   },
   {
-    path: '**',
-    redirectTo: 'home',
+    path: "campaigns",
+    loadComponent: () =>
+      import("./campaigns/campaigns-landing/campaigns-landing.component").then(
+        e => e.CampaignsLandingComponent
+      ),
+  },
+  {
+    path: "spells",
+    loadComponent: () =>
+      import("./spells/components/spells/spells.component").then(
+        e => e.SpellsComponent
+      ),
+  },
+  {
+    path: "feedback",
+    loadComponent: () =>
+      import("./feedback/feedback.component").then(m => m.FeedbackComponent),
+  },
+  {
+    path: "privacy",
+    loadComponent: () =>
+      import("./components/privacy-policy/privacy-policy.component").then(
+        c => c.PrivacyPolicyComponent
+      ),
+  },
+  {
+    path: "terms",
+    loadComponent: () =>
+      import("./components/terms/terms.component").then(c => c.TermsComponent),
+  },
+  {
+    path: "items",
+    loadComponent: () =>
+      import("./items/items.component").then(i => i.ItemsComponent),
+  },
+  {
+    path: "magic-item-tables",
+    loadComponent: () =>
+      import("./magic-item-tables/magic-item-tables.component").then(
+        c => c.MagicItemTablesComponent
+      ),
+  },
+  {
+    path: "printable-spell-list-generator",
+    loadComponent: () =>
+      import(
+        "./components/printable-spell-list-generator/printable-spell-list-generator.component"
+      ).then(c => c.PrintableSpellListGeneratorComponent),
+  },
+  {
+    path: "changelog",
+    loadComponent: () =>
+      import("./components/changelog/changelog.component").then(
+        c => c.ChangelogComponent
+      ),
+  },
+  {
+    path: "can-i-hit",
+    loadComponent: () =>
+      import("./components/can-i-hit/can-i-hit.component").then(
+        m => m.CanIHitComponent
+      ),
+  },
+  {
+    path: "characters",
+    loadChildren: () =>
+      import("./characters/characters.routes").then(m => m.routes),
+  },
+  {
+    path: "**",
+    redirectTo: "home",
   },
 ];
